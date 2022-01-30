fdae7ea52061c1f9ccfa99650702decb26c8c341
Hosna <hosna.hoseini@gmail.com>
2022-01-30 10:53:46 +0330

    refactor getting path

--- src/clean.py
+++ src/clean.py
@@ -12,11 +12,7 @@ class OrganizeFiles:
     This class is used to organize files in a directory.
     """
    
-    def __init__(self, directory):
-        self.directory = Path(directory)
-        if not self.directory.is_dir():
-            raise FileExistsError(f'{self.directory} is not a directory')
-        
+    def __init__(self):
 
         extensions = read_json(DATA_DIR / 'extensions.json')
         self.extensions_directories = {}
@@ -25,14 +21,17 @@ class OrganizeFiles:
                 self.extensions_directories[ext] = dir_name
         
 
-    def __call__(self):
+    def __call__(self, directory_path):
         """
         Organize files in the directory.
         regarding to file extentions
         """
+        directory = Path(directory_path)
+        if not directory.is_dir():
+            raise FileExistsError(f'{directory} is not a directory')
+        
         file_extentions = []
-
-        for file_path in self.directory.iterdir():
+        for file_path in directory.iterdir():
 
             if file_path.is_dir():
                 continue
@@ -44,15 +43,15 @@ class OrganizeFiles:
             file_extentions.append(file_path.suffix)
             
             if file_path.suffix not in self.extensions_directories:
-                DEST_DIR = self.directory / 'others'
+                DEST_DIR = directory / 'others'
             else:
-                DEST_DIR = self.directory / self.extensions_directories[file_path.suffix]
+                DEST_DIR = directory / self.extensions_directories[file_path.suffix]
             DEST_DIR.mkdir(exist_ok=True)
             logger.info(f'Moving {file_path} to {DEST_DIR}')
             shutil.move(str(file_path), str(DEST_DIR))
 
 
 if __name__ == '__main__':
-    organize_files = OrganizeFiles('/mnt/c/Users/RAI/Desktop/CS-tutorial/Projects/clean-directory/src/data/files')
-    organize_files()
+    organize_files = OrganizeFiles()
+    organize_files('/mnt/c/Users/RAI/Desktop/CS-tutorial/Projects/clean-directory/src/data/files')
        
